743303c7d886bed8646130fb79e626306f9e54f9
Matt McCarley <mdmccarley89@gmail.com>
2021-05-20 14:48:21 -0700

    Add initial files

---
+++ SesameStreet.java
@@ -0,0 +1,7 @@
+public class SesameStreet {
+    public static void main(String[] args) {
+        CookieMonster cm = new CookieMonster();
+        // Third diagram point
+        cm.eatCookie();
+    }
+}
